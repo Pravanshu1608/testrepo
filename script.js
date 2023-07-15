dae35fa2b3a00252904bb73d3f5f3d1387b834d7
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-07-15 23:03:08 +0530

    intial

---
+++ script.js
@@ -0,0 +1,8 @@
+const dropdown = document.querySelector('.dropdown');
+const opt1 = document.querySelector('.subopt1');
+const opt2 = document.querySelector('.subopt2')
+
+dropdown.addEventListener('click', () =>{
+    dropdown.classList.toggle('active');
+});
+
